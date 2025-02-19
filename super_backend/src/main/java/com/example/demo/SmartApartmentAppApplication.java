b16b2ca8bb901e4673d8266fa61cd840632a1adf
ruben <rubengarcia0515@gmail.com>
2025-02-18 19:59:51 -0600

    init

---
+++ super_backend/src/main/java/com/example/demo/SmartApartmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartApartmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartApartmentAppApplication.class, args);
+	}
+
+}
