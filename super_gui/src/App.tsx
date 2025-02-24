7d6e562fac4e455ea421c880c7c1642a83210322
Jennifer <jmarthagodfrey@gmail.com>
2025-02-24 13:09:10 -0700

    fixed merge issues

--- super_gui/src/App.tsx
+++ super_gui/src/App.tsx
@@ -1,25 +1,23 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import { StyleGuide } from "./pages/StyleGuide";
-import Header from "./components/Header";
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { StyleGuide } from './pages/StyleGuide';
 import { Complaint } from './components/complaint/Complaint';
+import Header from './components/Header';
 
 function App() {
-    return (
-        <>
-            <Header />
-            <Complaint />
-            <Router>
-                <a href="/StyleGuide" className="text-blue underline">
-                    Style Guide
-                </a>
-
-                <Routes>
-                    <Route path="/StyleGuide" element={<StyleGuide />} />
-                </Routes>
-            </Router>
-        </>
-    );
+  
+  return (
+    <>
+      <Header />
+      <Router>
+        <a href="/StyleGuide" className='text-blue underline'>Style Guide</a>
+        <Complaint />
+        <Routes>
+          <Route path="/StyleGuide" element={<StyleGuide />} />
+        </Routes>
+      </Router>
+    </> 
+  )
 }
 
 export default App;
