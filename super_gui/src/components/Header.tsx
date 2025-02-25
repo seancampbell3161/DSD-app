c9581acb0e4ac162f5b311bec528d033cde89262
Israel Santana <is.wsie@gmail.com>
2025-02-25 11:36:37 -0500

    feat: improved folder structure for fonts and assets.

--- super_gui/src/components/Header.tsx
+++ super_gui/src/components/Header.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-    return (
-        <header className='my-4 text-center'>
-            <h1 className='font-[Bebas_Neue] text-6xl text-[#D23715]'>super</h1>
-        </header>
-    );
+  return (
+    <header className="my-4 text-center">
+      <h1 className="font-[Bebas_Neue] text-6xl text-[#D23715]">super</h1>
+    </header>
+  );
 };
 
 export default Header;
--- super_gui/src/components/SmartLockUI.tsx
+++ super_gui/src/components/SmartLockUI.tsx
@@ -1,8 +1,20 @@
+// import { useState } from "react";
 
 const SmartLockUI = () => {
   return (
-    <div>SmartLockUI</div>
-  )
-}
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-6 justify-center gap-6">
+      <div className="flex justify-center">
+        <img className="rounded-2xl border" />
+        <div className="flex justify-center">
+          <div className="flex flex-col items-center mr-6 -my-4 whitespace-nowrap">
+            <h2 className="text-white text-xs md:text-base font-extrabold uppercase"></h2>
+            <h2 className=" text-white text-xs md:text-base ">Starting at $</h2>
+            <h2 className="text-white text-xs md:text-base ">Trims: FWD/AWD</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SmartLockUI
+export default SmartLockUI;
