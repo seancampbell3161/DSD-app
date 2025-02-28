6284dc84853120d4f5dc12dec2bfa3b5fa6c8ae5
Israel Santana <is.wsie@gmail.com>
2025-02-27 21:03:58 -0500

    feat: enhance SmartLockUI with dynamic menu items and responsive mobile navigation

--- super_gui/src/components/SmartLockUI.tsx
+++ super_gui/src/components/SmartLockUI.tsx
@@ -6,42 +6,38 @@ import SmartLocker from "/assets/icons/bxs-package.svg";
 import Key from "/assets/icons/bxs-key.svg";
 import Car from "/assets/icons/bxs-car.svg";
 import User from "/assets/icons/bx-user-circle.svg";
+import Lease from "/assets/icons/bxs-pen.svg";
 
 const SmartLockUI = () => {
-  return (
-    <main className="min-h-screen">
-      {/* NAV BAR */}
-      <div className="container mx-auto border-2 max-w-4xl">
-        <div className="border-b-2 border-t-2 p-2 flex justify-between">
-          <div className="flex gap-2 items-center mb-2 sm:mb-0 w-full sm:w-auto">
-            <Link to="" className="border rounded-full p-1 flex-shrink-0">
-              <img src={FrontDoor} alt="Front Door" className="w-6 h-6" />
-            </Link>
-            <p className="text-sm md:text-base">Front Door</p>
-          </div>
-          <div className="flex gap-2 items-center mb-2 sm:mb-0 w-full sm:w-auto">
-            <Link to="" className="border rounded-full p-1 flex-shrink-0">
-              <img src={Parking} alt="Parking" className="w-6 h-6" />
-            </Link>
-            <p className="text-sm md:text-base">Guest Parking</p>
-          </div>
-          <div className="flex gap-2 items-center mb-2 sm:mb-0 w-full sm:w-auto">
-            <Link to="" className="border rounded-full p-1 flex-shrink-0">
-              <img src={Complaints} alt="Complaints" className="w-6 h-6" />
-            </Link>
-            <p className="text-sm md:text-base">Complaints</p>
-          </div>
-          <div className="flex gap-2 items-center w-full sm:w-auto ml-6">
-            <Link
-              to=""
-              className="relative border rounded-full p-1 flex-shrink-0 "
-            >
-              <img src={SmartLocker} alt="Smart Locker" className="w-6 h-6" />
-            </Link>
-            <p className="text-sm md:text-base">Smart Locker</p>
-          </div>
-        </div>
+  const menuItems = [
+    { Icon: FrontDoor, alt: "FrontDoor" },
+    { Icon: Parking, alt: "Parking" },
+    { Icon: Complaints, alt: "Complaints" },
+    { Icon: Lease, alt: "Lease" },
+    { Icon: SmartLocker, alt: "SmartLocker" },
+  ];
 
+  return (
+    <main className="min-h-screen relative pb-16 md:pb-0">
+      {/* DESKTOP NAV BAR*/}
+      <div className="hidden md:block container mx-auto border-2 max-w-4xl">
+        <div className="border-b-2 border-t-2 p-2 flex justify-between">
+          {menuItems.map((item, index) => (
+            <div key={index} className="flex gap-2 items-center w-auto">
+              <Link
+                to=""
+                className="border rounded-full p-2 flex-shrink-0 bg-gray-800 hover:bg-accentGreen focus:bg-accentGreen focus:ring-0 focus:ring-accentGreen focus:outline-none transition duration-500 group"
+              >
+                <img src={item.Icon} alt={item.alt} className="w-6 h-6" />
+              </Link>
+              <p className="text-base">{item.alt}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* MAIN CONTENT */}
+      <div className="container mx-auto border-2 max-w-4xl">
         {/* LOCK ACCESS */}
         <div className="mx-auto m-2 p-2">
           <h2 className="text-xl font-bold mb-2">Hello Moto,</h2>
@@ -70,7 +66,7 @@ const SmartLockUI = () => {
 
           <div className="flex flex-col items-center gap-4">
             <button className="flex justify-center items-center gap-2 border rounded-sm w-full max-w-xs p-3">
-              <img src={Key} alt="Key" className="w-5 h-5" />
+              <img src={Key} alt="Key" className="w-5 h-5 " />
               Tap for Lock #
             </button>
 
@@ -80,6 +76,29 @@ const SmartLockUI = () => {
             </button>
           </div>
         </div>
+      </div>
+
+      {/* MOBILE NAV BAR */}
+      <div className="md:hidden fixed bottom-3 left-0 right-0 bg-white border-t-2 p-2">
+        <div className="flex justify-between max-w-4xl mx-auto">
+          {menuItems.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-col gap-2 items-center w-auto"
+            >
+              <Link
+                to=""
+                className="mt-2 border rounded-full p-2 flex-shrink-0 bg-gray-800 hover:bg-accentGreen focus:bg-accentGreen focus:ring-0 focus:ring-accentGreen focus:outline-none transition duration-500 group"
+              >
+                <img src={item.Icon} alt={item.alt} className="w-5 h-5 mb-" />
+              </Link>
+              {/* <p className="hidden sm:text-sm mt-1">{item.alt}</p> */}
+            </div>
+          ))}
+        </div>
+
+        {/* Push Icons 2px up */}
+        <div className="mt-2"></div>
       </div>
     </main>
   );
