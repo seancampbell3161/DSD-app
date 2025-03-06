610295ad5b764ca8fe704cac3fa0e2b865826f46
ruben <rubengarcia0515@gmail.com>
2025-03-05 23:12:32 -0600

    added repositories

---
+++ super_backend/src/main/java/com/example/demo/repository/TenantRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.entities.Tenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TenantRepository extends JpaRepository<Tenant, Long> {
+}
