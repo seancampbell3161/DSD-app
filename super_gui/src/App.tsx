6e8449fafbaf43dd3aef3e712ec13479c414a500
Israel Santana <is.wsie@gmail.com>
2025-03-07 20:16:46 -0500

    feat: update routing in App component to set SmartLockUI as the default view

--- super_gui/src/App.tsx
+++ super_gui/src/App.tsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { StyleGuide } from "./pages/StyleGuide";
-import { Complaint } from "./components/complaint/Complaint";
 import Header from "./components/Header";
 import SmartLockUI from "./components/SmartLockUI";
 
@@ -10,9 +8,7 @@ const App = () => {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/styleguide" element={<StyleGuide />} />
-        <Route path="/smartlockui" element={<SmartLockUI />} />
-        <Route path="/complaint" element={<Complaint />} />
+        <Route path="/" element={<SmartLockUI />} />
       </Routes>
     </BrowserRouter>
   );
