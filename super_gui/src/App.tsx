946ad1111ac7a210a06ec266a34a3d4d30cdd401
Kevin Deal <kevin@julyfivedesign.com>
2025-03-12 14:08:35 -0400

    login test

--- super_gui/src/App.tsx
+++ super_gui/src/App.tsx
@@ -4,21 +4,24 @@ import { StyleGuide } from "./pages/StyleGuide";
 import { Complaint } from "./components/complaint/Complaint";
 import Header from "./components/Header";
 import SmartLockUI from "./components/SmartLockUI";
+import { Login } from "./pages/login";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <nav className=" flex justify-center gap-8">
+      <nav className="flex justify-center gap-8">
         {/* Navigation component */}
         <Link to="/styleguide">Style Guide</Link>
         <Link to="/smartlockui">Smart Lock UI</Link>
         <Link to="/complaint">Complaint</Link>
+        <Link to="/login">Login</Link>
       </nav>
       <Routes>
         <Route path="/styleguide" element={<StyleGuide />} />
         <Route path="/smartlockui" element={<SmartLockUI />} />
         <Route path="/complaint" element={<Complaint />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
