9c93a91d1d6d44348e269a66f51bb26ba8bdd8b3
Israel Santana <is.wsie@gmail.com>
2025-03-12 15:36:09 -0400

    feat: add new routes for SmartLockUI, Complaint, Lease, and SmartLock components

--- super_gui/src/App.tsx
+++ super_gui/src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import SmartLockUI from "./components/SmartLockUI";
 import Activeparkpass from "./components/Activeparkpass";
+import Complaint from "./components/complaint/Complaint";
+import Lease from "./components/Lease";
+import SmartLock from "./components/SmartLock";
 import Nav from "./global/Nav";
 
 const App = () => {
@@ -10,7 +14,11 @@ const App = () => {
       <Header />
       <Nav />
       <Routes>
+        <Route path="/" element={<SmartLockUI />} />
         <Route path="/parking" element={<Activeparkpass />} />
+        <Route path="/complaint" element={<Complaint />} />
+        <Route path="/lease" element={<Lease />} />
+        <Route path="/smartlock" element={<SmartLock />} />
       </Routes>
     </BrowserRouter>
   );
