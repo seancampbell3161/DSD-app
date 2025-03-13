a8edf7c7fc0a689ce04053c442096b76dd05dbd8
ruben <rubengarcia0515@gmail.com>
2025-03-12 21:02:16 -0500

    resolved dependency

--- super_gui/src/App.tsx
+++ super_gui/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { StyleGuide } from "./pages/StyleGuide";
 import { Complaint } from "./components/complaint/Complaint";
 import Header from "./components/Header";
