9008662e4481ac25232cb836977d39dae9211104
ruben <rubengarcia0515@gmail.com>
2025-03-13 18:33:38 -0500

    added repository update for user

--- super_backend/src/main/java/com/example/demo/repository/UserRepository.java
+++ super_backend/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.repository;
 
 import com.example.demo.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailIgnoreCase(String email);
+    Page<User> findByEmailContainingIgnoreCase(String email, Pageable pageable);
 }
