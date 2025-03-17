c7808af914309640dcc75840dd8bb69df71f9b7b
AramWondergem <aramwondergem@gmail.com>
2025-03-17 12:05:36 -0400

    feat: example clean up database

--- super_backend/src/main/resources/data.sql
+++ super_backend/src/main/resources/data.sql
@@ -1,6 +1,9 @@
 DELETE FROM doors_users where (select count(*) from doors_users) > 0;
 DELETE FROM users_roles where (select count(*) from users_roles) > 0;
 DELETE FROM tenants_apartments where (select count(*) from tenants_apartments) > 0;
+DELETE FROM lease_tenants where (select count(*) from lease_tenants) > 0;
+DELETE FROM tenants_apartments where (select count(*) from tenants_apartments) > 0;
+DELETE FROM leases where (select count(*) from leases) > 0;
 DELETE FROM entry_codes where (select count(*) from entry_codes) > 0;
 DELETE FROM users where (select count(*) from users) > 0;
 DELETE FROM roles where (select count(*) from roles) > 0;
@@ -8,6 +11,8 @@ DELETE FROM doors where (select count(*) from doors) > 0;
 DELETE FROM parkings where(select count(*) from parkings) > 0;
 DELETE FROM apartments where (select count(*) from apartments) > 0;
 DELETE FROM buildings where (select count(*) from buildings) > 0;
+DELETE FROM tenants where (select count(*) from tenants) > 0;
+DELETE FROM complaints where (select count(*) from complaints) > 0;
 
 
 INSERT INTO roles (id,name) VALUES (1,'TENANT'),(2,'ADMIN');
