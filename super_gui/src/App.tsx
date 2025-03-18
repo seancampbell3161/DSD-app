65caed192c3f2d0389deed6873a17ad1c4687e68
funbunch <shannan_bunch@hotmail.com>
2025-03-17 19:22:15 -0700

    Revert App changes

--- super_gui/src/App.tsx
+++ super_gui/src/App.tsx
@@ -1,39 +1,30 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import SmartLockUI from "./components/SmartLockUI";
-import DigitalLease from "./components/DigitalLease";
 import { Login } from "./pages/login";
+import { Complaint } from "./components/complaint/Complaint";
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/styleguide" element={<StyleGuide />} />
-        <Route path="/smartlockui" element={<SmartLockUI />} />
-        <Route path="/complaint" element={<Complaint />} />
-        <Route path="/lease" element={<DigitalLease />} />
-      </Routes>
-    </BrowserRouter>
-    <>
-      <Login />
-      <BrowserRouter>
-        <Header />
-        <nav className="flex justify-center gap-8">
-          {/* Navigation component */}
-          <Link to="/styleguide">Style Guide</Link>
-          <Link to="/smartlockui">Smart Lock UI</Link>
-          <Link to="/complaint">Complaint</Link>
-        </nav>
-        <Routes>
-          <Route path="/styleguide" element={<StyleGuide />} />
-          <Route path="/smartlockui" element={<SmartLockUI />} />
-          <Route path="/complaint" element={<Complaint />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-};
+    const App = () => {
+      return (
+        <>
+          <Login />
+          <BrowserRouter>
+            <Header />
+            <nav className="flex justify-center gap-8">
+              {/* Navigation component */}
+              <Link to="/smartlockui">Smart Lock UI</Link>
+              <Link to="/complaint">Complaint</Link>
+              <Link to="/login">Login</Link>
+            </nav>
+            <Routes>
+              <Route path="/smartlockui" element={<SmartLockUI />} />
+              <Route path="/complaint" element={<Complaint />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </BrowserRouter>
+        </>
+    );
+    };
 
 export default App;
