8ebede86be80afcbc2038c80058913c44febdb76
funbunch <shannan_bunch@hotmail.com>
2025-03-18 18:16:13 -0700

    Add back in Digital Lease routes

--- super_gui/src/App.tsx
+++ super_gui/src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import SmartLockUI from "./components/SmartLockUI";
 import { Login } from "./pages/login";
 import { Complaint } from "./components/complaint/Complaint";
+import DigitalLease from "./components/DigitalLease";
 
     const App = () => {
       return (
@@ -16,11 +17,13 @@ import { Complaint } from "./components/complaint/Complaint";
               <Link to="/smartlockui">Smart Lock UI</Link>
               <Link to="/complaint">Complaint</Link>
               <Link to="/login">Login</Link>
+              <Link to="/lease">Digital Lease</Link>
             </nav>
             <Routes>
               <Route path="/smartlockui" element={<SmartLockUI />} />
               <Route path="/complaint" element={<Complaint />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/lease" element={<DigitalLease />} />
             </Routes>
           </BrowserRouter>
         </>
