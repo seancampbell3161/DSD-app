5a969a8dc7a2cc979eef8282861ffa969fa27bf0
Israel Santana <is.wsie@gmail.com>
2025-03-19 14:44:52 -0400

    feat: Replace SmartLockUI with FrontDoorUI, add package pickup notification
    - Renamed SmartLockUI to FrontDoorUI to better reflect its purpose.
    - Commented out the DigitalLease route since the file has not been added.
    - Implemented a toast notification in the tenant's smart lock view to inform tenant they have successfully picked up a package.

--- super_gui/src/App.tsx
+++ super_gui/src/App.tsx
@@ -2,11 +2,11 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Header from "./components/Header";
-import SmartLockUI from "./components/SmartLockUI";
+import FrontDoorUI from "./components/FrontDoorUI.tsx";
 import Activeparkpass from "./components/Activeparkpass";
 import { Login } from "./pages/login";
 import { Complaint } from "./components/complaint/Complaint";
-import DigitalLease from "./components/DigitalLease";
+// import DigitalLease from "./components/DigitalLease";
 import { Nav } from "./global/Nav.tsx";
 
 const App = () => {
@@ -17,10 +17,10 @@ const App = () => {
         <Header />
         <Nav />
         <Routes>
-          <Route path="/" element={<SmartLockUI />} />
+          <Route path="/" element={<FrontDoorUI />} />
           <Route path="/parking" element={<Activeparkpass />} />
           <Route path="/complaint" element={<Complaint />} />
-          <Route path="/lease" element={<DigitalLease />} />
+          {/* <Route path="/lease" element={<DigitalLease />} /> */}
         </Routes>
         <ToastContainer />
       </BrowserRouter>
