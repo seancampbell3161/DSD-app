c3dddb7558b64bfeb0ad7c14796d2aa3a2a5658d
Israel Santana <is.wsie@gmail.com>
2025-03-18 20:54:32 -0400

    Fixed: Merge conflicts between main branch and guest-park branch

--- super_gui/src/App.tsx
+++ super_gui/src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Header from "./components/Header";
 import SmartLockUI from "./components/SmartLockUI";
+import Activeparkpass from "./components/Activeparkpass";
 import { Login } from "./pages/login";
 import { Complaint } from "./components/complaint/Complaint";
 import Nav from "./global/Nav";
@@ -15,8 +17,10 @@ const App = () => {
         <Nav />
         <Routes>
           <Route path="/" element={<SmartLockUI />} />
+          <Route path="/parking" element={<Activeparkpass />} />
           <Route path="/complaint" element={<Complaint />} />
         </Routes>
+        <ToastContainer />
       </BrowserRouter>
     </>
   );
