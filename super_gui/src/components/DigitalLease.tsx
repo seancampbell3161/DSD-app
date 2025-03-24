34eb5422a24ff651fbe3f1e9eea9edf4b0906758
funbunch <shannan_bunch@hotmail.com>
2025-03-24 15:39:19 -0700

    Switching to one col test

--- super_gui/src/components/DigitalLease.tsx
+++ super_gui/src/components/DigitalLease.tsx
@@ -3,7 +3,7 @@ import AutoCompleteTenant from "./AutoCompleteSearch";
 const DigitalLease = () => {
 
     return (
-        <section className="max-w-4xl h-auto p-[40px] m-4 mx-auto text-center rounded-2xl bg-accentBlue">
+        <section className="max-w-4xl mx-auto">
             <AutoCompleteTenant />
         </section>
     );
