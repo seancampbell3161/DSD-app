9422fd78e58fd37f62eda48a543117306baf19f9
funbunch <shannan_bunch@hotmail.com>
2025-03-25 13:49:16 -0700

    Adding in refactored form

--- super_gui/src/components/Uploader.tsx
+++ super_gui/src/components/Uploader.tsx
@@ -2,9 +2,13 @@ import React, { useState } from "react"
 
 const FileUpload = ( {signerEmails}: {signerEmails: string} ) => {
 	const [selectedFile, setSelectedFile] = useState<File | null>(null)
-	const [status, setStatus] = useState<
-		"initial" | "uploading" | "success" | "fail"
-	>("initial")
+	const [status, setStatus] = useState<"initial" | "uploading" | "success" | "fail">("initial")
+  const [formData, setFormData] = useState({
+    apartmentNumber: '',
+    ccEmails: '',
+    startDate: '',
+    endDate: ''
+  })
 
 	interface LeaseSignatureRequestDetails {
 		signerEmails: string[]
@@ -19,19 +23,19 @@ const FileUpload = ( {signerEmails}: {signerEmails: string} ) => {
 		endDate: string
 	}
 	// Sample data, for testing purposes you can change the signer email to you own email to verify, but in production it should be the tenant's email which is passed over from the user search result passing in the email.
-	const leaseSignatureRequestDetails: LeaseSignatureRequestDetails = {
-		signerEmails: [ signerEmails ],
-		apartmentNumber: 2,
-		ccEmails: ["rubengarcia0515@gmail.com"],
-	}
+	// const leaseSignatureRequestDetails: LeaseSignatureRequestDetails = {
+	// 	signerEmails: [ signerEmails ],
+	// 	apartmentNumber: 2,
+	// 	ccEmails: ["rubengarcia0515@gmail.com"],
+	// }
 
-	const metaData: MetaData = {
-		title: "Sample File",
-		description: "This is a test file",
-		startDate: "2025-09-01",
-		endDate: "2026-08-31",
+	// const metaData: MetaData = {
+	// 	title: "Sample File",
+	// 	description: "This is a test file",
+	// 	startDate: "2025-09-01",
+	// 	endDate: "2026-08-31",
 
-	}
+	// }
 
 	const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		if (e.target.files) {
@@ -39,32 +43,39 @@ const FileUpload = ( {signerEmails}: {signerEmails: string} ) => {
 		}
 	}
 
-	const handleUpload = async () => {
+	const handleUpload = async ( e: React.FormEvent) => {
+    e.preventDefault()
+
 		if (!selectedFile) return
 
 		setStatus("uploading")
-		const fileContents = await selectedFile.arrayBuffer()
-		const file = new File([fileContents], selectedFile.name, {
-			type: selectedFile.type,
-		})
-		console.log("Original File Type:", file.type)
-    console.table(file)
+
+    const leaseSignatureRequestDetails = {
+      signerEmails: [signerEmails],
+      apartmentNumber: parseInt(formData.apartmentNumber) || 0,
+      ccEmails: [formData.ccEmails]
+    }
+
+    const metaData = {
+      title: "Tenant Lease",
+      description: "Lease document for tenant",
+      startDate: formData.startDate,
+      endDate: formData.endDate
+    }
 	
-		const formData = new FormData()
-		formData.append("file", selectedFile)
-		formData.append(
+		const formDataToSend = new FormData()
+		formDataToSend.append("file", selectedFile)
+		formDataToSend.append(
 			"leaseSignatureRequestDetails",
 			JSON.stringify(leaseSignatureRequestDetails),
 		)
-		formData.append("metaData", JSON.stringify(metaData))
+		formDataToSend.append("metaData", JSON.stringify(metaData))
 
-		console.log(file)
-    console.table(formData)
 
 		try {
 			const result = await fetch("/api/document/send", {
 				method: "POST",
-				body: formData,
+				body: formDataToSend,
 				headers: {
 					Accept: "application/json", 
 				},
@@ -84,80 +95,112 @@ const FileUpload = ( {signerEmails}: {signerEmails: string} ) => {
 		}
 	}
 
+  const handleProcess = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target
+
+    setFormData(prevData => ({
+      ...prevData,
+      [id]: value
+    }));
+    console.log(`Updated ${id} to: ${value}`)
+  }
+
 	return (
 		<>
 			<h3 className="text-xl mt-2.5 font-[Raleway]">Upload Tenant Lease</h3>
 			<p className="font-[var(--font-body)] text-sm text-[var(--color-grey-800)] italic mb-2.5">
 				pdf format
 			</p>
-			<div className="flex flex-col items-center justify-center">
-				<label
-					htmlFor="file"
-					className="cursor-pointer bg-[var(--color-blue)] text-[var(--color-white)] text-xl rounded-sm py-4 px-10 capitalize"
-				>
-					{selectedFile ? selectedFile.name : "Choose a file"}
-				</label>
-				<input
-					id="file"
-					type="file"
-					accept="application/pdf"
-					onChange={onFileChange}
-					className="hidden"
-				/>
-        <label
-					htmlFor="carbon-copy"
-					className="block mb-1 text-sm font-medium text-gray-700">
-            CC Email Address
-				</label>
-				<input
-					id="carbon-copy"
-					type="email"
-					onChange={onFileChange}
-					className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-				/>
-        <label
-					htmlFor="start"
-					className="block mb-1 text-sm font-medium text-gray-700">
-            Lease Start Date
-				</label>
-				<input
-					id="start"
-					type="date"
-					// value={formData.startDate}
-					onChange={onFileChange}
-					className="w-full px-3 py-2 text-black border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-				/>
-        <label
-					htmlFor="end"
-					className="block mb-1 text-sm font-medium text-gray-700">
-            Lease End Date
-				</label>
-				<input
-					id="end"
-					type="date"
-					onChange={onFileChange}
-					className="w-full px-3 py-2 text-black border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-				/>
-			</div>
-			{selectedFile && (
-				<section className="flex flex-col items-center mt-4 text-[var(--color-grey-800)]">
-					File details:
-					<ul>
-						<li>Name: {selectedFile.name}</li>
-						<li>Size: {selectedFile.size} bytes</li>
-					</ul>
-				</section>
-			)}
+      <form onSubmit={handleUpload}>
+        <div className="flex flex-col items-center justify-center">
+          <label
+            htmlFor="file"
+            className="cursor-pointer bg-[var(--color-blue)] text-[var(--color-white)] text-xl rounded-sm py-2 px-5 capitalize"
+          >
+            {selectedFile ? selectedFile.name : "Choose a file"}
+          </label>
+          <input
+            id="file"
+            type="file"
+            accept="application/pdf"
+            onChange={onFileChange}
+            className="hidden"
+            required
+          />
 
-			{selectedFile && (
-				<button
-					onClick={handleUpload}
-					className="mt-5 bg-[var(--color-blue)] text-white text-xl rounded-sm py-4 px-8 capitalize cursor-pointer"
-				>
-					Upload a file
-				</button>
-			)}
-			<Result status={status} />
+          <label
+            htmlFor="apartmentNumber"
+            className="block mb-1 text-sm font-medium text-gray-700">
+              Apartment Number
+          </label>
+          <input
+            id="apartmentNumber"
+            type="text"
+            onChange={handleProcess}
+            value={formData.apartmentNumber}
+            placeholder="Apartment Number"
+            required
+            className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <label
+            htmlFor="ccEmails"
+            className="block mb-1 text-sm font-medium text-gray-700">
+              CC Email Address
+          </label>
+          <input
+            id="ccEmails"
+            type="email"
+            placeholder="CC Email Address"
+            onChange={handleProcess}
+            value={formData.ccEmails}
+            className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+
+          <label
+            htmlFor="startDate"
+            className="block mb-1 text-sm font-medium text-gray-700">
+              Lease Start Date
+          </label>
+          <input
+            id="startDate"
+            type="date"
+            value={formData.startDate}
+            onChange={handleProcess}
+            className="px-3 py-2 text-black border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <label
+            htmlFor="endDate"
+            className="block mb-1 text-sm font-medium text-gray-700">
+              Lease End Date
+          </label>
+          <input
+            id="endDate"
+            type="date"
+            onChange={handleProcess}
+            value={formData.endDate}
+            className="px-3 py-2 text-black border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        {selectedFile && (
+          <section className="flex flex-col items-center mt-4 text-[var(--color-grey-800)]">
+            File details:
+            <ul>
+              <li>Name: {selectedFile.name}</li>
+              <li>Size: {selectedFile.size} bytes</li>
+            </ul>
+          </section>
+        )}
+
+        {selectedFile && (
+          <button
+            type="submit"
+            className="mt-5 bg-[var(--color-blue)] text-white text-xl rounded-sm py-4 px-8 capitalize cursor-pointer"
+          >
+            Upload a file
+          </button>
+        )}
+        <Result status={status} />
+      </form>
 		</>
 	)
 }
